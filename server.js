235d9dde7f1d875ad2ce574f2d25cba20423b584
Professor Marcelo <avalia.cesf.marcelo2024@gmail.com>
2024-08-02 12:20:00 -0300

    subir express01

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const server = express()
+
+server.listen(3000)
+
+server.get('/',(req,res)=>{
+    res.send('<h1>Olá ExpressJS</h1>')
+})
+
+server.get('/sobre',(req,res)=>{
+    res.send('<h2>Esse é o servidor do 3DS</h2>')
+})
+
+server.get('/servicos',(req,res)=>{
+    res.send('<h2>Esse é o servidor de serviço do 3DS</h2>')
+})
+
+
+server.use((req,res)=>{
+    res.status(404).send('<h2>Lamento, não encontramos esse recurso!</h2>')
+})
+
+
+
---
+++ server2.js
@@ -0,0 +1,38 @@
+const express = require('express')
+
+const server2 =express()
+
+server2.listen(3000)
+
+/*
+criando nosso padrão de rotas
+- principal
+- servicos
+- sobre
+- erro
+*/
+
+server2.get('/principal',(req,res)=>{
+    res.sendFile('./html/principal.html',{root: __dirname})
+
+})
+
+server2.get('/servicos',(req,res)=>{
+    res.sendFile('./html/servicos.html',{root: __dirname})
+    
+})
+
+server2.get('/sobre',(req,res)=>{
+    res.sendFile('./html/sobre.html',{root: __dirname})
+    
+})
+
+server2.get('/acerca',(req,res)=>{
+    res.redirect('/sobre')
+    
+})
+
+server2.use('/',(req,res)=>{
+    res.sendFile('./html/erro.html',{root: __dirname})
+    
+})
